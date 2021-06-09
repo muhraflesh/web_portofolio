1f2032c6483f5b7cad3616c54fa4d1af51e2dea4
muhraflesh <muhraflesh@gmail.com>
2021-06-09 10:50:14 +0700

    first commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/responsi-pemweb/';
+$config['base_url'] = 'https://portofolio-raflesh.herokuapp.com//';
 
 /*
 |--------------------------------------------------------------------------
